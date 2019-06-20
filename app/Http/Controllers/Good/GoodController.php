9b5f4e546a7e8aac8505c15f38fe26ee52e86c3a
zhangtao <1589506042@qq.com>
2019-06-20 12:52:25 +0800

    项目初始化

---
+++ app/Http/Controllers/Good/GoodController.php
@@ -0,0 +1,34 @@
+<?php 
+namespace App\Http\Controllers\Good;
+
+use App\Http\Controllers\Controller;
+
+use App\Model\User;
+
+class GoodController extends Controller
+{
+
+   public function index(){
+
+   	   echo 1;
+   	   echo '张涛';
+
+   }
+
+
+   public function aa(){
+
+   	echo 222;die;
+   }
+
+
+
+
+
+}
+
+
+
+
+
+ ?>
